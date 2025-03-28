461383b88d180eb23d023a7161f9fb1b47ef77fc
Priyang83 <patelpriyang83@gmail.com>
2025-03-28 22:44:09 +0530

    commit

--- dynamicDataDisplay/app.js
+++ dynamicDataDisplay/app.js
@@ -1,29 +1,41 @@
+let tempDataMain = data;
+let tCol = [];
 function displayBtn(sliceLen, arrayData) {
     // console.log(arrayData)
-    const dataLen = arrayData.length;
+    tempDataMain = arrayData;
+    const dataLen = tempDataMain.length;
     let noOfBtn = parseInt(dataLen / sliceLen);
-    
+
     if (dataLen % sliceLen != 0) {
         noOfBtn += 1;
     }
     // console.log(noOfBtn)
     let button_Array = [];
     for (let i = 1; i <= noOfBtn; i++) {
-        button_Array += `<button onclick="displayData('${sliceLen * i - sliceLen}', '${sliceLen * i}');" class="btn btn-primary"> Page ${i} </button>`
+        button_Array += `<button onclick="displayData(${sliceLen * i - sliceLen}, ${sliceLen * i});" class="btn btn-primary"> Page ${i} </button>`
     }
-    document.getElementById("display_btn").innerHTML=button_Array;
-    displayData(0,sliceLen)
+    document.getElementById("display_btn").innerHTML = button_Array;
+    displayData(0, sliceLen)
 }
-function displayData(start,end){
+function displayData(start, end) {
     // console.log(arrayData);
     
-    
-    const temp=data.slice(start,end).map(function(val){
-        let tempData=``;
-        for(let key in val){
+    tCol = [];
+    for (key in tempDataMain[0]) {
+        tCol.push(key);
+    }
+
+    const colName = tCol.map(function (val) {
+        return `<th>${val}</th>`;
+    })
+
+    //console.log(tCol);
+    const temp = tempDataMain.slice(start, end).map(function (val) {
+        let tempData = ``;
+        for (let key in val) {
             tempData += `
 
-                <td><button onclick="sortData('${key}');"><i class="fa-solid fa-arrow-up"></i></button><p>${val[key]}</p> </td>   
+                <td><div class="flexcontainer"><p>${val[key]}</p><button onclick="sortData('${key}');"><i class="fa-solid fa-arrow-up"></i></button></div> </td>   
               `;
         }
         return `
@@ -32,17 +44,53 @@ function displayData(start,end){
         </tr>
         `;
     });
-    document.getElementById("display_Data").innerHTML=temp.join(" ");
+    document.getElementById("displayCol").innerHTML = colName.join("")
+    document.getElementById("display_Data").innerHTML = temp.join(" ");
+    
 }
-function sortData(keyname){
-    data.sort(function(val,val2){
-        if(val[keyname] < val2[keyname]){
+
+function sortData(keyname) {
+    tempDataMain.sort(function (val, val2) {
+        if (val[keyname] < val2[keyname]) {
             return -1;
         }
-        else{
+        else {
             return 1;
         }
     });
-    displayBtn(10,data);
+    displayBtn(10, tempDataMain);
 }
+
 displayBtn(10, data);
+
+const colNameOption = tCol.map(function (val) {
+    return `<option value="${val}">${val}</option>`;
+});
+document.getElementById("colSelect").innerHTML = colNameOption.join("");
+
+function dataSearch() {
+    const cols = document.getElementById("colSelect").value;
+    // console.log(cols);
+    const searchSelection = document.getElementById("searchSelect").value;
+    const searchVal = document.getElementById("searchVal").value;
+    //console.log(searchSelection);
+    
+    let searchData = [];
+    if (searchSelection == "startsWith") {
+        searchData = data.filter(function (val) {
+            // console.log(val[cols])
+            //console.log(val[b])
+            let s = String(val[cols]);
+            return s.startsWith(searchVal)
+        })
+    }
+    else if (searchSelection == "endsWith") {
+        searchData = data.filter(function (val) {
+            let s = String(val[cols]);
+            return s.endsWith(searchVal);
+
+        })
+    }
+    displayBtn(10, searchData);
+
+}
